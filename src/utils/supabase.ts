09d8baa0e881fa9c0d73a0e70b6c721643362ec8
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2025-02-19 13:03:29 +0100

    fix variabili

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -8,8 +8,8 @@ export function getSupabaseBrowserClient() {
     return client
   }
   client = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
   )
 
   return client
