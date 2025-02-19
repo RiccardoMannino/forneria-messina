1ab59e10be3ba692369f0977d279547b9859b95f
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2025-02-19 14:06:51 +0100

    altro tentativo di fix

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,12 +4,9 @@ export type TypedSupabaseClient = SupabaseClient
 let client: TypedSupabaseClient | undefined
 
 export function getSupabaseBrowserClient() {
-  if (client) {
-    return client
-  }
   client = createClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL as string,
+    process.env.SUPABASE_ANON_KEY as string,
   )
 
   return client
